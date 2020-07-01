3497a525b50106dba5e590b447761bd36f51262f
michael <164770389@qq.com>
2020-07-01 23:45:16 +0800

    day 1

--- 02_variable_declaration.dart
+++ 02_variable_declaration.dart
@@ -6,9 +6,34 @@ main(List<String> args) {
   print(age);
   print(name);
 
-  final height = 20;
+  final height = 20; //can not change in following code
   const NAME = "YAO";
 
   print(height);
   print(NAME);
+
+  //during compile phase const need to know the exact value
+  //const curr = DateTime.now();   //error
+
+  // final p1 = Person('p');
+  // final p2 = Person('p');
+  // print(identical(p1, p2));  //false
+
+  const p1 = const Person('p'); //const/new
+  const p2 = Person('p');
+  print(identical(p1, p2)); //true
+}
+
+// class Person {
+//   String name;
+
+//   Person(String name) {
+//     this.name = name;
+//   }
+// }
+
+class Person {
+  final String name;
+
+  const Person(this.name);
 }
---
+++ 03_booltype.dart
@@ -0,0 +1,11 @@
+main(List<String> args) {
+  int age = 10;
+
+  //compile error. in dart we dont have that if not zero then true. not empty then true
+  // if (age) {
+
+  // }
+
+  bool flag = false;
+  if (flag) {}
+}
