0e9849f09da5fb4cf860f1ade9ad5c47e7555883
michael <164770389@qq.com>
2020-07-01 18:09:37 +0800

    first day learning

---
+++ 02_variable_declaration.dart
@@ -0,0 +1,14 @@
+main(List<String> args) {
+  String name = 'Michael';
+
+  var age = 20; // type should be int can not set other type value like 'String'
+
+  print(age);
+  print(name);
+
+  final height = 20;
+  const NAME = "YAO";
+
+  print(height);
+  print(NAME);
+}
